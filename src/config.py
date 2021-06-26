24e4dadecd768c0df2fe306b4e317ecda73d6a36
DukeNgn <triduc2311m@gmail.com>
2021-06-26 19:19:51 -0400

    Add DOH training

--- src/config.py
+++ src/config.py
@@ -4,4 +4,5 @@ import os
 
 load_dotenv()
 
-DATASET_PATH = Path(os.environ.get("dataset", "./Forbes2013.csv"))
+FORBES_DATASET_PATH = Path(os.environ.get("forbes-dataset", "./Forbes2013.csv"))
+DOH_DATASET_PATH = Path(os.environ.get("DOH-dataset", "./DOH2020_combined.csv"))
